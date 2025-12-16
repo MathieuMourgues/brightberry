21abfd048c5e603d18012d891b6f5e076ff41bbf
Lelyondepolytech <mmourgues03@gmail.com>
2025-12-16 19:53:37 +0100

    Revert "reduction"
    This reverts commit 6587029026911d8d764d1b046c8fce33696dfc8d.

--- app/artwork/[id]/page.tsx
+++ app/artwork/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/firebaseConfig";
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 import Image from "next/image";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
 
